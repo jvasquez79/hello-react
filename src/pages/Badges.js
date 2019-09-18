28131263caf3f31f7495a8c7752d55ec2b106b89
Javier Vasquez <jvasquez79@gmail.com>
2019-09-18 13:09:57 -0500

    Terminando edición y refrescando automáticamente

--- src/pages/Badges.js
+++ src/pages/Badges.js
@@ -5,6 +5,7 @@ import confLogo from '../images/badge-header.svg';
 import BadgesList from '../components/BadgesList';
 import PageLoading from '../components/PageLoading';
 import PageError from '../components/PageError';
+import MiniLoader from '../components/MiniLoader';
 import api from '../api'
 import { Link } from 'react-router-dom';
 //import BadgeForm from '../components/BadgeForm';
@@ -83,6 +84,12 @@ class Badges extends React.Component {
 
     componentDidMount() {
         this.fetchData();
+
+        this.intervalId = setInterval(this.fetchData, 5000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.intervalId);
     }
 
     fetchData = async () => {
@@ -98,7 +105,7 @@ class Badges extends React.Component {
 
     render() {
         //console.log('2/4. render()');
-        if (this.state.loading === true) {
+        if (this.state.loading === true && !this.state.data) {
             //return 'Loading...';
             return <PageLoading />
         }
@@ -126,6 +133,8 @@ class Badges extends React.Component {
                 <div className="Badges__list">
                     <div className="Badges__container">
                         <BadgesList badges={this.state.data} />
+
+                        {this.state.loading && <MiniLoader />}
                     </div>
                 </div>
             </React.Fragment>
