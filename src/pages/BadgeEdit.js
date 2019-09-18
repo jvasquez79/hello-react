07b22d294cc1b1cd30ff6c5fd9f5c0addf15fbbb
Javier Vasquez <jvasquez79@gmail.com>
2019-09-18 12:46:00 -0500

    Finalizando edición de datos con PUT

--- src/pages/BadgeEdit.js
+++ src/pages/BadgeEdit.js
@@ -10,7 +10,7 @@ import api from '../api';
 
 class BadgeEdit extends React.Component {
     state = { 
-        loading: false,
+        loading: true,
         error: null,
         form: {
             firstName: '',
@@ -20,6 +20,23 @@ class BadgeEdit extends React.Component {
             twitter: '',
         } 
     };
+
+    componentDidMount() {
+        this.fetchData();
+    }
+
+    fetchData = async e => {
+        this.setState({ loading: true, error: null} );
+        try {
+            const data = await api.badges.read(
+                this.props.match.params.badgeId
+            );
+
+            this.setState({ loading: false, form: data });
+        } catch(error) {
+            this.setState({ loading: false, error: error });
+        }
+    }
 
     handleChange = e => {
         this.setState( {
@@ -35,7 +52,7 @@ class BadgeEdit extends React.Component {
         this.setState({ loading: true, error: null});
 
         try {
-            await api.badges.create(this.state.form);
+            await api.badges.update(this.props.match.params.badgeId, this.state.form);
             this.setState({ loading: false });
 
             this.props.history.push('/badges');
@@ -67,6 +84,7 @@ class BadgeEdit extends React.Component {
                         </div>
 
                         <div className="col-6">
+                            <h1>Edit Attendant</h1>
                             <BadgeForm 
                                 onChange={this.handleChange} 
                                 onSubmit={this.handleSubmit}
